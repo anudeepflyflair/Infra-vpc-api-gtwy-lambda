6a658daef9f527243484049605bf9d83e236ab75
Anudeep Yesupogu <anudeep.yesupogu@valuelabs.com>
2024-12-24 21:58:03 +0530

    modified

--- import.tf
+++ import.tf
@@ -1,9 +1,9 @@
 import {
-  id = "arn:aws:lambda:ca-central-1:017820679929:function:PaymentGateway_IntelysisToken"
+  id = "PaymentGateway_IntelysisToken"
   to = aws_lambda_function.PaymentGateway_IntelysisToken
 }
 
 import {
-  id = "arn:aws:lambda:ca-central-1:017820679929:function:PaymentGateway_Orchestrator"
+  id = "PaymentGateway_Orchestrator"
   to = aws_lambda_function.PaymentGateway_Orchestrator
 }
